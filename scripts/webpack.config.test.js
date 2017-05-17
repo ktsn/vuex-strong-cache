8ba7e4d80e71f993c57b5695d63036122c526792
ktsn <ktsn55@gmail.com>
2017-05-18 00:01:30 +0900

    Inject promise polyfill for test

--- scripts/webpack.config.test.js
+++ scripts/webpack.config.test.js
@@ -2,7 +2,8 @@ const path = require('path')
 const glob = require('glob')
 
 module.exports = {
-  entry: glob.sync(path.resolve(__dirname, '../test/**/*.spec.ts')),
+  entry: ['es6-promise/auto']
+    .concat(glob.sync(path.resolve(__dirname, '../test/**/*.spec.ts'))),
   output: {
     path: path.resolve(__dirname, '../.tmp'),
     filename: 'test.js'
